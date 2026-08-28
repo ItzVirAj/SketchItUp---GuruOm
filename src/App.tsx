d20465e3dc88426038b5856617730205418249ff
ItzVirAj <virajm0812@gmail.com>
2026-08-28 20:23:10 +0530

    fix: minor bugs fixed

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ export default function App() {
       <AccentThemeProvider>
         <ToastProvider>
           <MainApp />
-          <Agentation />
+          {import.meta.env.DEV && <Agentation />}
         </ToastProvider>
       </AccentThemeProvider>
     </AuthProvider>
